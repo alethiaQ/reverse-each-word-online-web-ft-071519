edd71ef5dde99c7e6cb57a8be2d95b56523de89b
Alethia Quintero <alethiaq22@gmail.com>
2019-07-16 17:25:45 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,7 @@
 def reverse_each_word(string)
 my_arr = string.split
-new_arr = []
-my_arr.each do |word|
-  new_arr << word.reverse
+my_arr.collect do |word|
+  word.reverse
 end
-new_arr.join(" ")
+my_arr
 end
